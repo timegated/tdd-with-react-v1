e3c9a5f76d983e6c3d7f2bee86204d8fbce96cb2
timegated <beccdaniel@gmail.com>
2022-03-18 11:41:44 -0700

    Using jest mock fn

--- testing-react-components/src/components/Form/Form.test.js
+++ testing-react-components/src/components/Form/Form.test.js
@@ -4,8 +4,10 @@ import userEvent from '@testing-library/user-event';
 import Form from './Form';
 
 test('submitting the form calls onsubmit with username and password', () => {
-  let submittedData;
-  const handleSubmit = (data) => submittedData = data;
+  const handleSubmit = jest.fn();
+  // let submittedData;
+  // can mock this. built into jest.
+  // const handleSubmit = (data) => submittedData = data;
   const username = 'chucknorris';
   const password = 'i need no password';
   render(<Form onSubmit={handleSubmit}/>);
@@ -13,8 +15,9 @@ test('submitting the form calls onsubmit with username and password', () => {
   userEvent.type(screen.getByLabelText(/password/i), password);
   userEvent.click(screen.getByRole('button', {name: /submit/i}));
 
-  expect(submittedData).toEqual({
+  expect(handleSubmit).toHaveBeenCalledWith({
     username,
     password,
   });
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
 });
