2dad8dac9c0595e6af581bb8e7b4f3c7a2937ccd
timegated <beccdaniel@gmail.com>
2022-03-18 15:29:41 -0700

    Added jackfrankling/test-data-bot library for faking data

--- testing-react-components/src/components/Form/Form.test.js
+++ testing-react-components/src/components/Form/Form.test.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { build, fake } from '@jackfranklin/test-data-bot';
 import Form from './Form';
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  }
+});
+
+
+// Test object factory function
+// function buildLoginForm(overrides) {
+//   return {
+//     username: faker.internet.userName(),
+//     password: faker.internet.password(),
+//     ...overrides,
+//   };
+// }
 
 test('submitting the form calls onsubmit with username and password', () => {
   const handleSubmit = jest.fn();
   // let submittedData;
   // can mock this. built into jest.
   // const handleSubmit = (data) => submittedData = data;
-  const username = 'chucknorris';
-  const password = 'i need no password';
-  render(<Form onSubmit={handleSubmit}/>);
+  // static values, not super important
+  const { username, password } = buildLoginForm();
+
+  console.log({ username, password });
+
+  render(<Form onSubmit={handleSubmit} />);
   userEvent.type(screen.getByLabelText(/username/i), username);
   userEvent.type(screen.getByLabelText(/password/i), password);
-  userEvent.click(screen.getByRole('button', {name: /submit/i}));
+  userEvent.click(screen.getByRole('button', { name: /submit/i }));
 
   expect(handleSubmit).toHaveBeenCalledWith({
     username,
