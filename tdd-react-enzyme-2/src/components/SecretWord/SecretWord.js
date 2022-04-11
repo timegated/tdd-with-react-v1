6d045c0491ebb2893be547ae5a55c9ebc929b607
timegated <beccdaniel@gmail.com>
2022-04-11 09:14:47 -0700

    updated snapshotsand rendering logic for SecretWord (updated the base state object)

--- tdd-react-enzyme-2/src/components/SecretWord/SecretWord.js
+++ tdd-react-enzyme-2/src/components/SecretWord/SecretWord.js
@@ -4,10 +4,11 @@ import { randomWordState } from '../../store/selectors';
 
 const SecretWord = () => {
   const secretWord = useRecoilValue(randomWordState);
+  const { word } = secretWord;
   console.log(`%c ${secretWord}`, 'font-size:18px; color:orange;');
   return (
     <div>
-      <h1>{secretWord.toUpperCase()}</h1>
+      <h1>{word.toUpperCase()}</h1>
     </div>
   );
 };
